c3d864f1e55d73e3c4b89808cab94aa2c892053f
sarapalaciod <sara.palaciod@udea.edu.com>
2024-05-13 16:12:01 -0500

    creacion clase cargar_paciente

--- clases.py
+++ clases.py
@@ -1,8 +1,18 @@
+import cv2
+import pydicom
+import matplotlib.pyplot as plt
+import os
+from pydicom.data import get_testdata_file
+from pydicom import dcmread
+import nibabel as nib
+import numpy as np
+
+
 class Paciente:
     def __init__(self):
     
         self.__nombre = ""
-        self.__id = int
+        self.__ID = int
         self.__edad = int
         self.__imagen = ""
         
@@ -23,3 +33,20 @@ class Paciente:
         self.__edad = g
     def asignarId(self,c):
         self.__id = c
+
+    def cargar_paciente(lectura):
+        if n.endswith('.dcm'):  
+                lectura = os.path.join(archivo, n)
+                dcm = pydicom.dcmread(lectura)
+                
+                nombre=dcm.PatientName
+                edad= dcm.PatientAge
+                ID=dcm.PatientID
+
+                im = dcm.pixel_array
+                imagen_nifti = nib.Nifti1Image(im, np.eye(4))
+                nombre_nifti = os.path.splitext(n)[0] + ".nii.gz"
+                nib.save(imagen_nifti, nombre_nifti)
+                return ' el paciente se cargo efectivamente'
+
+    
--- implementacion.py
+++ implementacion.py
@@ -6,7 +6,7 @@ from pydicom.data import get_testdata_file
 from pydicom import dcmread
 import nibabel as nib
 import numpy as np
-from clases import *
+import clases 
 dicc_archivos={}
 dicc_pacientes={}
 p=Paciente
@@ -22,22 +22,13 @@ def main():
 
         if menu==1:
             archivo=input("ingrese nombre de la carpeta con archivos dicom")
-            #arch_dicom=[]
-            print(os.listdir(archivo))
-            if n.endswith('.dcm'):  
-                lectura = os.path.join(archivo, n)
-                dcm = pydicom.dcmread(lectura)
-                #arch_dicom.append(dcm)
-                im = dcm.pixel_array
-                imagen_nifti = nib.Nifti1Image(im, np.eye(4))
-                nombre_nifti = os.path.splitext(n)[0] + ".nii.gz"
-                nib.save(imagen_nifti, nombre_nifti)
+            cargar_paciente(archivo)
+            dicc_pacientes[ID]=p 
+            dicc_archivos[archivo]=nombre_nifti
+           
+            
 
-                nombre = input("Ingrese el nombre: ")
-                id = int(input("Ingrese la cedula: "))
-                edad= int(input("ingrese la edad"))
-
-                dicc_archivos[id]=[nombre,edad]
+        
 
                 # plt.imshow(im)
                 # plt.show()
@@ -50,5 +41,7 @@ def main():
             pass
         elif menu==5:
             break
+menu()
+
 
 
