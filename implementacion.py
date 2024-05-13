67ccceaf2ab734e977265cd6a1f51f87bdd5fa0a
MarianaGG3 <mariana.giraldog@udea.edu.co>
2024-05-13 13:31:07 -0500

    menu

--- implementacion.py
+++ implementacion.py
@@ -6,3 +6,23 @@ from pydicom.data import get_testdata_file
 from pydicom import dcmread
 
 
+while True:
+    menu=int(input("""
+    1. ingresar paciente
+    2. ingresar imagenes
+    3. hacer transformacion geometrica
+    4. manipulacion de imagenes"""))
+
+
+    if menu==1:
+        pass
+    elif menu==2:
+        pass
+    elif menu==3:
+        pass
+    elif menu==4:
+        pass
+    elif menu==5:
+        pass
+
+
