72f14e57accb78071c35389ac05a17320ade8da7
MarianaGG3 <mariana.giraldog@udea.edu.co>
2024-05-13 15:02:41 -0500

    opcion 1-leer carpeta,convertir nifti

--- implementacion.py
+++ implementacion.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import os
 from pydicom.data import get_testdata_file
 from pydicom import dcmread
+import nibabel as nib
+import numpy as np
 
 def main():
     while True:
@@ -16,7 +18,21 @@ def main():
 
 
         if menu==1:
-            pass
+            archivo=input("ingrese nombre de la carpeta con archivos dicom")
+            arch_dicom=[]
+            print(os.listdir(archivo))
+            if n.endswith('.dcm'):  
+                lectura = os.path.join(archivo, n)
+                dcm = pydicom.dcmread(lectura)
+                arch_dicom.append(dcm)
+                im = dcm.pixel_array
+                imagen_nifti = nib.Nifti1Image(im, np.eye(4))
+                nombre_nifti = os.path.splitext(n)[0] + ".nii.gz"
+                nib.save(imagen_nifti, nombre_nifti)
+
+                # plt.imshow(im)
+                # plt.show()
+
         elif menu==2:
             pass
         elif menu==3:
