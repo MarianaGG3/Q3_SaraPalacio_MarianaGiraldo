0ab330bcf30485c07a13aeb4ac94d5d30a234cb4
sarapalaciod <sara.palaciod@udea.edu.com>
2024-05-13 14:26:08 -0500

    menu

--- implementacion.py
+++ implementacion.py
@@ -5,24 +5,25 @@ import os
 from pydicom.data import get_testdata_file
 from pydicom import dcmread
 
+def main():
+    while True:
+        menu=int(input("""
+        1. Ingresar paciente
+        2. Ingresar imagenes
+        3. Hacer transformacion geometrica
+        4. Manipulacion de imagenes
+        5. Salir"""))
 
-while True:
-    menu=int(input("""
-    1. ingresar paciente
-    2. ingresar imagenes
-    3. hacer transformacion geometrica
-    4. manipulacion de imagenes"""))
 
-
-    if menu==1:
-        pass
-    elif menu==2:
-        pass
-    elif menu==3:
-        pass
-    elif menu==4:
-        pass
-    elif menu==5:
-        pass
+        if menu==1:
+            pass
+        elif menu==2:
+            pass
+        elif menu==3:
+            pass
+        elif menu==4:
+            pass
+        elif menu==5:
+            break
 
 
