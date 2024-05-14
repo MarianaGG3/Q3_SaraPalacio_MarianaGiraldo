e1dc1332f3eb46c09c62db29ec35dbffed0ffe1f
MarianaGG3 <mariana.giraldog@udea.edu.co>
2024-05-13 22:19:13 -0500

    opcion 1 en clases

--- clases.py
+++ clases.py
@@ -6,7 +6,8 @@ from pydicom.data import get_testdata_file
 from pydicom import dcmread
 import nibabel as nib
 import numpy as np
-
+dicc_archivos={}
+dicc_pacientes={}
 
 class Paciente:
     def __init__(self):
@@ -34,25 +35,67 @@ class Paciente:
     def asignarId(self,c):
         self.__id = c
 
-    def cargar_paciente(self, archivo):
-        if archivo.endswith('.dcm'):  
+    def rotar_imagen(self, key, grados):
+        img = dicc_archivos[key]
+        img_rotada = np.rot90(img, int(grados) // 90)
+        fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+        axs[0].imshow(img, cmap='gray')
+        axs[0].set_title('Imagen Original')
+        axs[0].axis('off')
+        axs[1].imshow(img_rotada, cmap='gray')
+        axs[1].set_title(f'Imagen Rotada {grados}°')
+        axs[1].axis('off')
+        nombre_rotada = f"{os.path.splitext(key)[0]}_rotada.png"
+        cv2.imwrite(nombre_rotada, img_rotada)
+        plt.show()
+
+    def procesar_imagen(self, key, umb, kernel_tam):
+        img = dicc_archivos[key].copy()
+        _, binarizada = cv2.threshold(img, int(umb), 255, cv2.THRESH_BINARY)
+        kernel = np.ones((int(kernel_tam), int(kernel_tam)), np.uint8)
+        morfologia = cv2.morphologyEx(binarizada, cv2.MORPH_OPEN, kernel)
+        texto = f"Imagen binarizada, Umbral: {umb}, Tamaño de kernel: {kernel_tam}"
+        color = (255, 255, 255)  # Color blanco
+        grosor = 2
+        altura, anchura = morfologia.shape[:2]
+        (ancho_texto, alto_texto), _ = cv2.getTextSize(texto, cv2.FONT_HERSHEY_SIMPLEX, 1, grosor)
+        x = (anchura - ancho_texto) // 2
+        y = altura - alto_texto - 10
+        cv2.putText(morfologia, texto, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, color, grosor, cv2.LINE_AA)
+        cv2.imshow("Imagen procesada", morfologia)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        cv2.imwrite("imagen_procesada.png", morfologia)
+
+class Cargar:
+    def cargar_paciente(self, archivo, dicc_pacientes, dicc_archivos):
+        for n in os.listdir(archivo):
+            if n.endswith('.dcm'):
                 lectura = os.path.join(archivo, n)
                 dcm = pydicom.dcmread(lectura)
                 
-                nombre=dcm.PatientName
-                edad= dcm.PatientAge
-                ID=dcm.PatientID
+                paciente = Paciente()
+                paciente.asignarNombre(dcm.PatientName)
+                paciente.asignarEdad(dcm.PatientAge)
+                paciente.asignarId(dcm.PatientID)
+                paciente.asignarImagen(lectura)
 
+                dicc_pacientes[paciente.verId()] = {
+                    "Nombre": paciente.verNombre(),
+                    "Edad": paciente.verEdad(),
+                    "Imagen": paciente.verImagen()
+                }
+                
                 im = dcm.pixel_array
                 imagen_nifti = nib.Nifti1Image(im, np.eye(4))
                 nombre_nifti = os.path.splitext(n)[0] + ".nii.gz"
                 nib.save(imagen_nifti, nombre_nifti)
+                dicc_archivos[nombre_nifti] = imagen_nifti
+                dicc_archivos[lectura] = dcm
+
+
+
+                
                 
 
-    def rotar_imagen(key, grados):
-        img= dicc_archivos[key] 
-    def procesar_imagen(key,umb,kernel_tam):
-        img= dicc_archivos[key] 
-        binarizada = cv2.threshold(img, umb, 255, cv2.THRESH_BINARY)
-        kernel = np.ones((kernel_tam, kernel_tam), np.uint8)
-        morfologia = cv2.morphologyEx(binarizada, cv2.MORPH_OPEN, kernel)
+    
--- implementacion.py
+++ implementacion.py
@@ -8,9 +8,9 @@ import nibabel as nib
 import numpy as np
 from clases import *
 
-dicc_archivos={}
-dicc_pacientes={}
-p=Paciente
+
+p=Paciente()
+c=Cargar()
 
 #def main():
 while True:
@@ -23,49 +23,29 @@ while True:
 
 
     if menu==1:
-        archivo=input("ingrese nombre de la carpeta con archivos dicom:  ")
-        
-        #print(os.listdir(archivo))
-        for n in os.listdir(archivo):
-            if n.endswith('.dcm'):  
-                lectura = os.path.join(archivo, n)
-                dcm = pydicom.dcmread(lectura)
-                dcm[0x0010 , 0x0010]
-                im = dcm.pixel_array
-                imagen_nifti = nib.Nifti1Image(im, np.eye(4))
-                nombre_nifti = os.path.splitext(n)[0] + ".nii.gz"
-                nib.save(imagen_nifti, nombre_nifti)
-
-                nombre=dcm.PatientName
-                edad= dcm.PatientAge
-                id=dcm.PatientID
-
-        dicc_archivos[id]=[nombre,edad]
-        print(dicc_archivos)
+         if menu == 1:
+            archivo = input("Ingrese nombre de la carpeta con archivos DICOM: ")
+            c.cargar_paciente(archivo, dicc_pacientes, dicc_archivos)
+            print("Pacientes cargados:")
+            print(dicc_pacientes)
 
         # plt.imshow(im)
         # plt.show()
 
-    elif menu==2:
-        archivo= input('Ingrese nombre del archivo de la imagen:  ')
-        key=input('crear clave de la imagen: ')
-        ima=cv2.imread(archivo)
-        dicc_archivos[key]=ima
-
-    elif menu==3:
-        key=input('ingrese la clave de la imagen que desea rotar: ')
-        grados=input('elige a que grados deseas rotar la imagen.\n 1. 90° \n 2. 180° \n 3. 270°')
-        rotar_imagen(key, grados)
-
-    elif menu==4:
-        key=input('ingrese la clave de la imagen que desea rotar: ')
-        umb= input('Ingrese el numero de umbral que tendra la imagen: ')
-        kernel=input('Ingrese el numero del kernel que tendra la imagen: ')
-        procesar_imagen(key, umb,kernel)
-
-    elif menu==5:
+    elif menu == 2:
+        archivo = input('Ingrese nombre del archivo de la imagen: ')
+        key = input('crear clave de la imagen: ')
+        ima = cv2.imread(archivo)
+        dicc_archivos[key] = ima
+        print(dicc_archivos)
+    elif menu == 3:
+        key = input('ingrese la clave de la imagen que desea rotar: ')
+        grados = input('cuantos grados deseas rotar la imagen 90°, 180° o 270°: ')
+        p.rotar_imagen(key, grados)
+    elif menu == 4:
+        key = input('ingrese la clave de la imagen que desea modificar: ')
+        umb = input('Ingrese el numero de umbral que tendra la imagen: ')
+        kernel = input('Ingrese el numero del kernel que tendra la imagen: ')
+        p.procesar_imagen(key, umb, kernel)
+    elif menu == 5:
         break
-#menu()
-
-
-
